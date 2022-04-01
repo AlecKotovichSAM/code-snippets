1ff582ef88a5508097cf6cf7bf3b84e7c44947b9
AlecKotovichSAM <AlecKotovichSAM@users.noreply.github.com>
2022-04-01 09:16:51 +0300

    Update backgroundLoading.java

--- backgroundLoading.java
+++ backgroundLoading.java
@@ -1,6 +1,7 @@
 boolean backgroundLoading = true;
-             Image image = new Image(url, backgroundLoading);
-        ImageView imageView = new ImageView(image);
-stackPane.getChildren().add(imageView);
-        
-        stackPane.setStyle("-fx-background-color: #3270B7");
+
+Image image = new Image(url, backgroundLoading);
+ImageView imageView = new ImageView(image);
+
+stackPane.getChildren().add(imageView);       
+stackPane.setStyle("-fx-background-color: #3270B7");
